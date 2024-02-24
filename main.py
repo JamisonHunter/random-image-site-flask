fb9b7167c9e906c3b7fa40f530a862b7bf0fb465
Jamison Hunter <126278717+JamisonHunter@users.noreply.github.com>
2024-02-24 00:12:40 +0000

    Basic Image Generation Is Functional

---
+++ main.py
@@ -0,0 +1,33 @@
+# Flask app.py
+from flask import Flask, render_template, jsonify
+import requests
+
+app = Flask(__name__, static_folder='static')
+
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+
+@app.route('/random-image')
+def random_image():
+  width = 1440
+  height = 1440
+  response = requests.get(f"https://picsum.photos/{width}/{height}")
+  if response.status_code == 200:
+    image_url = response.url
+    print(f"Image URL: {image_url}")
+    return jsonify({'image_url': image_url})
+  else:
+    print(f"Failed to get image URL. Status code: {response.status_code}")
+    return jsonify({'error': 'Failed to fetch image URL'}), 500
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8080, debug=True)
